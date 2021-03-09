60230d33ab8ea0e6887a0b523820d76c4295c042
Marie <71872231+marie-clerc@users.noreply.github.com>
2021-03-09 09:32:11 +0100

    fonction tri sans sort

--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -1,3 +1,4 @@
+/**
 function tri (numbers, order)
 {
     if ( order === "asc") {
@@ -15,3 +16,37 @@ function tri (numbers, order)
 }
 console.log(tri([40, 100, 1, 5, 25, 10], "desc"));
 console.log(tri([40, 100, 1, 5, 25, 10], "asc"));
+*/
+
+function tri(numbers, order) {
+
+    let newArray = numbers;
+    let temp = numbers;
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 0; j < numbers.length; j++) {
+            switch (order) {
+                case 'desc':
+                    if (numbers[i] > newArray[j]) {
+                        temp = numbers[i];
+                        numbers[i] = newArray[j];
+                        newArray[j] = temp;
+                    }
+                    break;
+                case 'asc':
+                    if (numbers[i] < newArray[j]) {
+                        temp = numbers[i];
+                        numbers[i] = newArray[j]
+                        newArray[j] = temp;
+                    }
+                    break;
+            }
+
+
+        }
+    }
+    return console.log(newArray);
+}
+
+numbers = [5, 8, 10, 13, 2, 3, 4, 6, 7]; // on donne un tableau prédéfini 
+tri(numbers, 'asc');
