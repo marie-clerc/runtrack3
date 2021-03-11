5f01aaf57221c69ca8a0c073a17f3e76c14210aa
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2021-03-11 13:33:28 +0100

    jsonValueKey()

---
+++ jour04/job02/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour04</title>
+</head>
+<body>
+    <!-- JS -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
