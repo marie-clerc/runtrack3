3f05a6b9fc3420c266e016703a7d00e9fda2fa15
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2021-03-15 15:52:31 +0100

    media queries navigation hamburger

--- jour06/job04/index.php
+++ jour06/job04/index.php
@@ -35,8 +35,6 @@
         4
     </section>
 </main>
-    <!-- JS -->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script type="text/javascript" src="script.js"></script>
+
 </body>
 </html>
