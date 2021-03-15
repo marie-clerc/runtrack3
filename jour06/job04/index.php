4c202a4a658214cf3bffb91f12ea2281236c7f8c
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2021-03-15 15:43:54 +0100

    media queries avec flex box

---
+++ jour06/job04/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen" href="style1.css" />
+</head>
+<body>
+<header class="header">
+    <a href="" class="logo">CSS Nav</a>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
+        <li><a href="#work">Accueil</a></li>
+        <li><a href="#about">Presentation</a></li>
+        <li><a href="#careers">Contact</a></li>
+    </ul>
+</header>
+<main>
+    <div>
+        Lorsque la résolution est inférieure ou égale à 767. Les liens
+        disparaissent pour laisser place à un hamburger button. Lorsque l’on clique
+        sur celui-ci, les liens apparaissent. Si on re-clique, les liens disparaissent.
+    </div>
+    <section class="section">
+        1
+    </section>
+    <section class="section">
+        1
+    </section>
+    <section class="section">
+        1
+    </section>
+    <section class="section">
+        1
+    </section>
+</main>
+    <!-- JS -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
