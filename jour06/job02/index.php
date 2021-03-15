9bf397e8e54621b52c96361d5afe5683dbcf1a92
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2021-03-15 11:17:45 +0100

    media queries avec un fichier css

---
+++ jour06/job02/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <meta name="viewpoint" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="screen" href="style1.css" />
+</head>
+<body>
+yo mama a hoe
+</body>
+</html>
