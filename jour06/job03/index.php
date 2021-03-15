bdde56f7b8f7dc49d314b6c0845284eafe58f991
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2021-03-15 11:41:00 +0100

    media queries avec flex box

---
+++ jour06/job03/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <meta name="viewpoint" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="screen" href="style1.css" />
+</head>
+<body>
+<section class="section">
+yo
+</section>
+<section class="section">
+mama
+</section>
+<section class="section">
+a
+</section>
+<section class="section">
+hoe
+</section>
+</body>
+</html>
