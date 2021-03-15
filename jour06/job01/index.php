7a1f8304f0f20cd166f78761470281c084444bb8
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2021-03-15 11:09:01 +0100

    media queries

---
+++ jour06/job01/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <meta name="viewpoint" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="screen" href="style1.css" />
+    <link rel="stylesheet" media="screen and (min-width: 1680px) and (max-width: 1920px)" href="style2.css" />
+    <link rel="stylesheet" media="screen and (min-width: 1280px) and (max-width: 1680px)" href="style3.css" />
+    <link rel="stylesheet" media="screen and (min-width: 1280px)" href="style4.css" />
+</head>
+<body class="html">
+yo mama a hoe
+<div class="caca">
+    j'en ai deja marre
+</div>
+</body>
+</html>
